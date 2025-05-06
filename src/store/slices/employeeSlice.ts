fb716fece7f485b17ce61ca8ee6b6ace46cb2e79
wrappixelTeam <cyberashish321@gmail.com>
2025-05-06 06:30:25 +0530

    updated

--- src/store/slices/employeeSlice.ts
+++ src/store/slices/employeeSlice.ts
@@ -2,29 +2,31 @@ import { Employee } from "@/components/dashboard/admin/all-employees/employeeDat
 import { createSlice } from "@reduxjs/toolkit";
 
 const initialState:Employee = {
-    name:"",
-    email:"",
-    gender:"male",
-    mobile_number:"",
-    aadhaar_number:"",
-    account_number:"",
-    department:"",
-    designation:"",
-    previous_company:"",
-    pf_number:"",
-    salary:"",
-    current_address:"",
-    permanent_address:"",
-    employeeDOBDate:"",
-    employeeJoiningDate:""
-  }
+    name: "",
+    email: "",
+    gender: "male",
+    mobile_number: "",
+    aadhaar_number: "",
+    account_number: "",
+    department: "",
+    designation: "",
+    previous_company: "",
+    pf_number: "",
+    salary: "",
+    current_address: "",
+    permanent_address: "",
+    employeeDOBDate: "",
+    employeeJoiningDate: "",
+    active: false,
+    leave: 0
+}
 
 const employeeSlice = createSlice({
     name: "employeeData",
     initialState,
     reducers: {
         setSelectedEmployee : (state,action) => {
-             return state = action.payload
+             return  {...state , ...action.payload}
         } 
     }
 });
