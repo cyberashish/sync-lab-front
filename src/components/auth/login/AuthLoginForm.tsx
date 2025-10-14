7560d2e78a41eeaad7c8f9e56b1506e90efb1b13
wrappixelTeam <cyberashish321@gmail.com>
2025-10-15 03:56:32 +0530

    updated

--- src/components/auth/login/AuthLoginForm.tsx
+++ src/components/auth/login/AuthLoginForm.tsx
@@ -50,6 +50,7 @@ export default function AuthLoginForm(){
     function handleGoogleLogin(){
       setLoading(true);
       window.open(`https://sync-lab-backend-cwqc.onrender.com/auth/google` , '_self')
+      // window.open(`http://localhost:8080/auth/google` , '_self')
     }
 
     return (
