8f4e4d714f305e516d58e33a102c709c1ea00363
wrappixelTeam <cyberashish321@gmail.com>
2025-10-31 00:59:22 +0530

    updated

--- src/components/dashboard/employee/holiday-list/HolidayList.tsx
+++ src/components/dashboard/employee/holiday-list/HolidayList.tsx
@@ -28,7 +28,7 @@ export default function HolidayList() {
   }
 
   return (
-    <div className="max-w-2xl mx-auto p-4">
+    <div className="max-w-3xl mx-auto p-4">
       <h2 className="text-2xl font-semibold mb-6 text-center dark:text-white">
         Corporate Holiday List ({new Date().getFullYear()})
       </h2>
